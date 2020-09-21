3208eae38931e4a0775639e22afa0ed8dfd20e51
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-21 16:30:13 -0500

    migrations done for users, posts, and post-images

--- database/migrations/20200921161301_users.js
+++ database/migrations/20200921161301_users.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (users) => {
+    users.increment();
+    users.string("username", 255).notNullable().unique();
+    users.string("password", 255).notNullable();
+    users.string("email", 255).notNullable();
+    users.string("location", 255);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('users')
 };
---
+++ database/migrations/20200921162727_post-images.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("post-images", (images) => {
+    images.increment();
+    images.string("url", 255).notNullable();
+    images.string("alt", 100).notNullable();
+    images
+      .integer("posts_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("posts");
+  });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('post-images')
+};
