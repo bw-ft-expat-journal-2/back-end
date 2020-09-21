47370db097694915a053ea6dee50072878735ac5
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-21 09:49:06 -0500

    completed auth middleware

---
+++ auth/auth-middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("./secrets");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    const secret = secrets.jwtSecret;
+    jwt.verify(token, secret, function (err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "invalid token" });
+      } else {
+        req.token = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "please login" });
+  }
+};
