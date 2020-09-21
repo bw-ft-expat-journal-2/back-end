3dc97d4cdb81c9862d2f891bdc6727def0c05cd1
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-21 16:17:40 -0500

    create images router as well

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const authenticate = require("../auth/auth-middleware");
 
 const usersRouter = require('../users/users-router')
 const postsRouter = require('../posts/posts-router')
-// const imagesRouter = require('')
+const imagesRouter = require('../images/images-router')
 
 const server = express();
 
@@ -22,6 +22,6 @@ server.get("/", (req, res) => {
 server.use("/api/auth", authRouter);
 server.use('/api/users', authenticate, usersRouter)
 server.use('/api/posts', authenticate, postsRouter)
-// server.use('/api/images', authenticate, imagesRouter)
+server.use('/api/images', authenticate, imagesRouter)
 
 module.exports = server;
---
+++ images/images-model.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+
+const Images = require("./images-router");
+
+router.get("/", (req, res) => {
+  Images.getImages()
+    .then((images) => {
+      res.status(200).json({ data: images });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Images.findById()
+    .then((image) => {
+      if (!image) {
+        res.status(404).json({ message: "image not found" });
+      } else {
+        res.status(200).json({ data: image });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.update("/:id", (req, res) => {
+  const updatedInfo = req.body;
+  Images.update(updatedInfo)
+    .then((image) => {
+      res.status(200).json({ data: image });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Images.remove()
+    .then((image) => {
+      if (!image) {
+        res.status(404).json({ message: "image not found" });
+      } else {
+        res.status(200).json({ message: "image successfully deleted" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
