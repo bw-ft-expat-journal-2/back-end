cc8accd17d4fbc14bc39b85be6d4f17db4da88f9
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-23 17:38:55 -0400

    fixed issue with posts post request, just need user id

--- posts/posts-model.js
+++ posts/posts-model.js
@@ -27,7 +27,7 @@ function findById(id) {
 function add(post) {
 	return db('posts').insert(post, 'id').then((ids) => {
 		const [ id ] = ids;
-		return findById({ id });
+		return findById(id);
 	});
 }
 
